228a511b9e7e1be4d86927a078af07b9b4766286
Ayush Bendwal <31592180+Ayushbendwal3@users.noreply.github.com>
2020-05-27 20:22:52 +0530

    First Commit

--- Modules/music.py
+++ Modules/music.py
@@ -1,9 +1,13 @@
 import os
 import random
 
-
+# please update your music directory path
 def play():
-    music_dir = 'C:\\Users\\admin\\Music\\'
-    songs = os.listdir(music_dir)
-    song = random.choice(songs)
-    os.startfile(os.path.join(music_dir, song))
+    try:
+        music_dir = 'C:\\Users\\admin\\Music\\'
+        songs = os.listdir(music_dir)
+        song = random.choice(songs)
+        os.startfile(os.path.join(music_dir, song))
+    except:
+        speak("Music files doesn't exists")
+    
